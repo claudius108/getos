9582bd73de60f583649a8de099098ea879dc53f1
claudius <claudius.teodorescu@gmail.com>
2017-09-24 16:54:49 +0300

    First corect build.

--- src/main/java/ro/kuberam/getos/Main.java
+++ src/main/java/ro/kuberam/getos/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import ro.kuberam.getos.controllers.GetosController;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -12,11 +13,14 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("/ro/kuberam/getos/getos.fxml"));
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/ro/kuberam/getos/app.fxml"));
 		BorderPane pane = loader.load();
 		Scene scene = new Scene(pane, 650, 400);
 		primaryStage.setScene(scene);
+		GetosController controller = ((GetosController) loader.getController());
 
+		controller.setStage(primaryStage);
+		
 		primaryStage.show();
 	}
 
--- src/main/java/ro/kuberam/getos/controllers/GetosController.java
+++ src/main/java/ro/kuberam/getos/controllers/GetosController.java
@@ -2,16 +2,13 @@ package ro.kuberam.getos.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 
-public class getosController implements Initializable {
-
-	@FXML // fx:id="quitButton"
-	private MenuItem quitButton; // Value injected by FXMLLoader
+public class GetosController implements Initializable {
 
 	private Stage stage;
 
@@ -20,17 +17,14 @@ public class getosController implements Initializable {
 		stage.close();
 	}
 
-	@FXML // This method is called by the FXMLLoader when initialization is
-			// complete
-	void initialize() {
-		assert quitButton != null : "fx:id=\"quitButton\" was not injected: check your FXML file 'app.fxml'.";
-
+	@FXML
+	void aboutAction(ActionEvent event) {
+		AboutDialog aboutDialog = new AboutDialog(stage);
+		aboutDialog.showAbout();
 	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-
 	}
 
 	public void setStage(Stage stage) {
