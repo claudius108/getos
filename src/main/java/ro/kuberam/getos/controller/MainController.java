0e8bf0f9b5de1c8ce953b2217472c7d845498b60
claudius <claudius.teodorescu@gmail.com>
2017-09-25 21:15:19 +0300

    Simplified menuHelpAbout().

--- src/main/java/ro/kuberam/getos/controller/MainController.java
+++ src/main/java/ro/kuberam/getos/controller/MainController.java
@@ -1,17 +1,12 @@
 package ro.kuberam.getos.controller;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.application.HostServices;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class MainController implements Initializable {
@@ -25,17 +20,8 @@ public class MainController implements Initializable {
 	}
 
 	@FXML
-	void menuHelpAbout(ActionEvent event) throws IOException {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("/ro/kuberam/getos/dialogs/about-dialog.fxml"));
-		Parent dialogRoot = loader.load();
-		
-		AboutDialogController dialogController = loader.getController();
-		dialogController.setHostServices(hostServices);
-		Stage dialog = new Stage();
-		dialog.setScene(new Scene(dialogRoot));
-		dialog.initModality(Modality.APPLICATION_MODAL);
-		
-		dialog.show();
+	void menuHelpAbout(ActionEvent event) throws Exception {
+		AboutDialogController.create(hostServices);
 	}
 
 	@Override
