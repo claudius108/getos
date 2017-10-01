b46f3c37a4e2133442d3a14530506a89efbcb46e
claudius <claudius.teodorescu@gmail.com>
2017-10-01 16:17:54 +0300

    Improved ControllerFactory.

--- src/main/java/ro/kuberam/getos/controller/factory/ControllerFactory.java
+++ src/main/java/ro/kuberam/getos/controller/factory/ControllerFactory.java
@@ -5,26 +5,41 @@ import java.lang.reflect.InvocationTargetException;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.util.Callback;
+import ro.kuberam.getos.modules.editorTab.EditorTabController;
+import ro.kuberam.getos.modules.viewers.ViewerFileType;
 
 public final class ControllerFactory implements Callback<Class<?>, Object> {
 
 	private final Application mApplication;
 	private final Stage mStage;
+	private final ViewerFileType viewerFileType;
 
 	public ControllerFactory(Application application) {
 		mApplication = application;
 		mStage = null;
+		viewerFileType = null;
 	}
 
 	public ControllerFactory(Application application, Stage stage) {
 		mApplication = application;
 		mStage = stage;
+		viewerFileType = null;
+	}
+
+	public ControllerFactory(Application application, Stage stage, ViewerFileType type) {
+		mApplication = application;
+		mStage = stage;
+		viewerFileType = type;
 	}
 
 	@Override
 	public Object call(Class<?> type) {
 		try {
-			if (StageController.class.isAssignableFrom(type)) {
+			if (EditorTabController.class.isAssignableFrom(type)) {
+				// EditorTabController is final so we don't need to guess the
+				// ctor
+				return new EditorTabController(mApplication, mStage, viewerFileType);
+			} else if (StageController.class.isAssignableFrom(type)) {
 				// Call the class first ctor
 				return type.getConstructors()[0].newInstance(mApplication, mStage);
 			} else if (Controller.class.isAssignableFrom(type)) {
