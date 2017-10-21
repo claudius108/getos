1421fd01cddf3c7cd2b235992dd28d368a5a242b
claudius <claudius.teodorescu@gmail.com>
2017-10-21 19:00:15 +0300

    Added map for the main events.

--- src/ro/kuberam/getos/Getos.java
+++ src/ro/kuberam/getos/Getos.java
@@ -1,14 +1,19 @@
 package ro.kuberam.getos;
 
+import java.util.HashMap;
+
 import javafx.application.Application;
+import javafx.event.Event;
 import javafx.stage.Stage;
 import ro.kuberam.getos.modules.eventBus.EventBus;
 import ro.kuberam.getos.modules.eventBus.FXEventBus;
 import ro.kuberam.getos.modules.main.MainWindowController;
 
 public class Getos extends Application {
-	
+
 	public static EventBus mainEventBus;
+
+	public static HashMap<String, Event> mainEvents = new  HashMap<String, Event>();
 
 	static {
 		mainEventBus = new FXEventBus();
